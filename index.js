b6898825f5d957f9afe8321c70452d00f7cd8ce0
Wissididom <30803034+Wissididom@users.noreply.github.com>
2022-10-28 19:04:11 +0200

    Removed unnecessary if check for 24h
    It only posted if the update were more than 24h in the past

--- index.js
+++ index.js
@@ -24,21 +24,19 @@ fetch('https://github.com/Chatterino/chatterino2/releases.atom').then(res => res
 			let differenceHours = differenceMinutes / 60;
 			console.log(`${differenceSeconds} / 60 = ${differenceMinutes} / 60 = ${differenceHours}`);
 			fs.readFile('lastUpdatedValue', async (err, data) => {
-				if (data != updated) {
-					if (differenceHours >= 24) {
-						console.log('There is a new version!');
-						let latestCommitMessage = await fetch('https://github.com/Chatterino/chatterino2/commits/master.atom').then(res => res.text()).then(text => {
-							return xml2js.parseStringPromise(text);
-						}).then(xmlCommitObj => {
-							return xmlCommitObj['feed']['entry'][0]['content'][0]['_'].replace(/(<([^>]+)>)/gi, '').trim();
-						}).catch(err => console.error(err));
-						let webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL});
-						webhookClient.send({
-							username: 'Chatterino Nightly',
-							avatarURL: 'https://camo.githubusercontent.com/6ca305d42786c9dbd0b76f5ade013601b080d71a598e881b4349dff2eafae6c7/68747470733a2f2f666f757274662e636f6d2f696d672f63686174746572696e6f2d69636f6e2d36342e706e67',
-							content: `New Nightly Version (Updated: <t:${timestamp}>):\nLatest Commit Message: \`${latestCommitMessage}\`\nChangelog (Unversioned): <${changelogLink}>\nLink: <${nightlyLink}>\nOSX: <${nightlyOsxLink}>\nWindows: <${nightlyWinLink}>\nAppImage: <${nightlyAppImageLink}>\nDEB: <${nightlyDebLink}>`
-						}).catch(err => console.error(err));
-					}
+				if (data.toString().trim() != updated) {
+					console.log('There is a new version!');
+					let latestCommitMessage = await fetch('https://github.com/Chatterino/chatterino2/commits/master.atom').then(res => res.text()).then(text => {
+						return xml2js.parseStringPromise(text);
+					}).then(xmlCommitObj => {
+						return xmlCommitObj['feed']['entry'][0]['content'][0]['_'].replace(/(<([^>]+)>)/gi, '').trim();
+					}).catch(err => console.error(err));
+					let webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL});
+					webhookClient.send({
+						username: 'Chatterino Nightly',
+						avatarURL: 'https://camo.githubusercontent.com/6ca305d42786c9dbd0b76f5ade013601b080d71a598e881b4349dff2eafae6c7/68747470733a2f2f666f757274662e636f6d2f696d672f63686174746572696e6f2d69636f6e2d36342e706e67',
+						content: `New Nightly Version (Updated: <t:${timestamp}>):\nLatest Commit Message: \`${latestCommitMessage}\`\nChangelog (Unversioned): <${changelogLink}>\nLink: <${nightlyLink}>\nOSX: <${nightlyOsxLink}>\nWindows: <${nightlyWinLink}>\nAppImage: <${nightlyAppImageLink}>\nDEB: <${nightlyDebLink}>`
+					}).catch(err => console.error(err));
 				} else {
 					console.log('Already latest version!');
 				}
