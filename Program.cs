3a5fabdd2b3e3ea70b7a95fb65797bd8b2432d3b
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-09-14 12:41:49 +0200

    Make the commit author a link and handle Co-Authors (#13)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace ChatterinoNightlyUpdateCheckerForDiscord
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Xml;
     using System.Text;
@@ -11,7 +12,7 @@
         public const string WEBHOOK_USERNAME = "Chatterino Nightly";
         public const string WEBHOOK_AVATAR_URL = "https://user-images.githubusercontent.com/41973452/272541622-52457e89-5f16-4c83-93e7-91866c25b606.png";
         public const string NIGHTLY_LINK = "https://github.com/Chatterino/chatterino2/releases/tag/nightly-build";
-        public const string CONTENT_FORMAT_STRING = "New Nightly Version (Updated: <t:{0}:F>):\nLatest Commit Message: ``{1}`` by ``{2}``\nLink: <{3}>";
+        public const string CONTENT_FORMAT_STRING = "New Nightly Version (Updated: <t:{0}:F>):\nLatest Commit Message: ``{1}`` by {2}\nLink: <{3}>";
 
         public static async Task Main(string[] args)
         {
@@ -37,7 +38,9 @@
                         Console.WriteLine("Needs update");
                         XmlNode entry = doc.GetElementsByTagName("entry")[0]!;
                         string title = "N/A";
-                        string author = "N/A";
+                        string? authorName = null;
+                        string? authorUrl = null;
+                        List<string> coauthors = new List<string>();
                         foreach (XmlNode node in entry.ChildNodes)
                         {
                             if (node.Name == "title") title = node.InnerText.Trim();
@@ -45,9 +48,63 @@
                             {
                                 foreach (XmlNode option in node.ChildNodes)
                                 {
-                                    if (option.Name == "name") author = option.InnerText.Trim();
+                                    if (option.Name == "name") authorName = option.InnerText.Trim();
+                                    if (option.Name == "uri") authorUrl = option.InnerText.Trim();
                                 }
                             }
+                            if (node.Name == "content")
+                            {
+                                foreach (string line in node.InnerText.Split('\n'))
+                                {
+                                    if (line.StartsWith("Co-authored-by:"))
+                                    {
+                                        string coauthor = line.Substring("Co-authored-by:".Length);
+                                        coauthor = coauthor.Substring(0, coauthor.IndexOf("&lt;")).Trim();
+                                        coauthors.Add(coauthor);
+                                    }
+                                }
+                            }
+                        }
+                        string? author = null;
+                        if (authorName is null)
+                        {
+                            if (authorUrl is null)
+                            {
+                                author = "N/A";
+                            }
+                            else
+                            {
+                                author = $"<{authorUrl}>";
+                            }
+                        }
+                        else
+                        {
+                            if (authorUrl is null)
+                            {
+                                author = $"``{authorName}``";
+                            }
+                            else
+                            {
+                                author = $"[{authorName}](<{authorUrl}>)";
+                            }
+                        }
+                        if (coauthors.Count > 0)
+                        {
+                            author += " (with ";
+                            bool first = true;
+                            foreach (string coauthor in coauthors)
+                            {
+                                if (first)
+                                {
+                                    author += $"``{coauthor}``";
+                                }
+                                else
+                                {
+                                    author += $", ``{coauthor}``";
+                                }
+                                first = false;
+                            }
+                            author += ")";
                         }
                         if (author != "dependabot")
                             Console.WriteLine((await PostDiscordMessage(client, timestamp, title, author)).StatusCode);
@@ -60,7 +117,7 @@
                 if (fileNeedsUpdate) File.WriteAllText("lastUpdatedValue", updated);
             }
         }
-        
+
         private static async Task<HttpResponseMessage> PostDiscordMessage(HttpClient client, long timestamp, string title, string author)
         {
             string url = $"{Environment.GetEnvironmentVariable("DISCORD_WEBHOOK_URL")}?wait=true";
