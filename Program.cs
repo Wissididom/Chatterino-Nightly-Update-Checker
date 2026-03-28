4879d93388cacf9876f8141559475040b12f51d3
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-03-28 22:42:55 +0100

    Make coauthor handling more reliable and fix formatting

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ namespace ChatterinoNightlyUpdateChecker
     public static partial class Program
     {
         private const string WebhookUsername = "Chatterino Nightly";
-        private const string WebhookAvatarUrl = "https://user-images.githubusercontent.com/41973452/272541622-52457e89-5f16-4c83-93e7-91866c25b606.png";
+
+        private const string WebhookAvatarUrl =
+            "https://user-images.githubusercontent.com/41973452/272541622-52457e89-5f16-4c83-93e7-91866c25b606.png";
+
         private const string NightlyLink = "https://github.com/Chatterino/chatterino2/releases/tag/nightly-build";
-        private const string ContentFormatString = "New Nightly Version (Updated: <t:{0}:F>):\nLatest Commit: {1} by {2}\nLink: <{3}>\nPull Request: {4}";
+
+        private const string ContentFormatString =
+            "New Nightly Version (Updated: <t:{0}:F>):\nLatest Commit: {1} by {2}\nLink: <{3}>\nPull Request: {4}";
 
         public static async Task Main(string[] args)
         {
@@ -18,12 +23,15 @@ namespace ChatterinoNightlyUpdateChecker
             client.DefaultRequestHeaders.Accept.Add(new("application/vnd.github+json"));
             client.DefaultRequestHeaders.Add("User-Agent", "Wissididom/Chatterino-Nightly-Update-Checker");
             client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2026-03-10");
-            var commits = JsonSerializer.Deserialize<ListCommitsResponse>(await client.GetStringAsync("https://api.github.com/repos/Chatterino/chatterino2/commits?sha=nightly-build").ConfigureAwait(false));
+            var commits = JsonSerializer.Deserialize<ListCommitsResponse>(await client
+                .GetStringAsync("https://api.github.com/repos/Chatterino/chatterino2/commits?sha=nightly-build")
+                .ConfigureAwait(false));
             if (commits is null)
             {
                 Console.WriteLine("Failed to parse json!");
                 return;
             }
+
             var commit = commits[0];
             var updated = commit.Commit?.Committer?.Date;
             if (updated is null)
@@ -31,6 +39,7 @@ namespace ChatterinoNightlyUpdateChecker
                 Console.WriteLine("Failed to get latest commit date!");
                 return;
             }
+
             var timestamp = ((DateTimeOffset)updated).ToUnixTimeSeconds().ToString();
             var fileNeedsUpdate = true;
             if (File.Exists("lastUpdatedValue"))
@@ -53,13 +62,22 @@ namespace ChatterinoNightlyUpdateChecker
                     }
                     else
                     {
-                        commitString = commit.HtmlUrl is null ? $"``{commitTitle}``" : $"[``{commitTitle}``](<{commit.HtmlUrl}>)";
+                        commitString = commit.HtmlUrl is null
+                            ? $"``{commitTitle}``"
+                            : $"[``{commitTitle}``](<{commit.HtmlUrl}>)";
                     }
+
                     var coauthors = new List<string>();
                     if (!string.IsNullOrEmpty(description))
                     {
-                        coauthors.AddRange(from line in description.Split('\n') where line.StartsWith("Co-authored-by:") select line["Co-authored-by:".Length..] into coauthor select coauthor[..coauthor.IndexOf('<', StringComparison.InvariantCulture)].Trim());
+                        coauthors.AddRange(description.Split('\n').Where(line => line.StartsWith("Co-authored-by:"))
+                            .Select(line => line["Co-authored-by:".Length..].Trim()).Select(coauthor =>
+                            {
+                                var i = coauthor.IndexOf('<', StringComparison.InvariantCulture);
+                                return i >= 0 ? coauthor[..i].Trim() : coauthor;
+                            }));
                     }
+
                     var authorName = commit.Commit?.Author?.Name ?? commit.Commit?.Committer?.Name;
                     string author;
                     if (authorName is null)
@@ -68,8 +86,11 @@ namespace ChatterinoNightlyUpdateChecker
                     }
                     else
                     {
-                        author = commit.Author?.HtmlUrl is null ? $"``{authorName}``" : $"[{authorName}](<{commit.Author.HtmlUrl}>)";
+                        author = commit.Author?.HtmlUrl is null
+                            ? $"``{authorName}``"
+                            : $"[{authorName}](<{commit.Author.HtmlUrl}>)";
                     }
+
                     if (coauthors.Count > 0)
                     {
                         author += " (with ";
@@ -84,23 +105,31 @@ namespace ChatterinoNightlyUpdateChecker
                             {
                                 author += $", ``{coauthor}``";
                             }
+
                             first = false;
                         }
+
                         author += ")";
                     }
+
                     if (authorName != "dependabot" && authorName != "dependabot[bot]")
-                        Console.WriteLine((await PostDiscordMessage(client, timestamp, commitString, author, description)).StatusCode);
+                        Console.WriteLine(
+                            (await PostDiscordMessage(client, timestamp, commitString, author, description))
+                            .StatusCode);
                 }
+
                 if (fileNeedsUpdate) await File.WriteAllTextAsync("lastUpdatedValue", timestamp);
             }
             else
             {
                 Console.WriteLine("File does not exist");
             }
+
             if (fileNeedsUpdate) await File.WriteAllTextAsync("lastUpdatedValue", timestamp);
         }
 
-        private static async Task<HttpResponseMessage> PostDiscordMessage(HttpClient client, string timestamp, string commit, string author, string description)
+        private static async Task<HttpResponseMessage> PostDiscordMessage(HttpClient client, string timestamp,
+            string commit, string author, string description)
         {
             var url = $"{Environment.GetEnvironmentVariable("DISCORD_WEBHOOK_URL")}?wait=true";
             var pr = "`N/A`";
@@ -111,17 +140,20 @@ namespace ChatterinoNightlyUpdateChecker
                 var prNumber = match.Groups[1].Value;
                 pr = $"[#{prNumber}](<https://github.com/Chatterino/chatterino2/pull/{prNumber}>)";
             }
+
             Console.WriteLine(ContentFormatString, timestamp, commit, author, NightlyLink, pr);
             var dcContent = string.Format(ContentFormatString, timestamp, commit, author, NightlyLink, pr);
             if (!string.IsNullOrEmpty(description))
             {
                 dcContent += $"\nDescription:\n```\n{description}\n```";
             }
+
             var webhookData = new WebhookData
             {
                 Username = WebhookUsername,
                 AvatarUrl = WebhookAvatarUrl,
-                AllowedMentions = new Dictionary<string, string[]>{
+                AllowedMentions = new Dictionary<string, string[]>
+                {
                     { "parse", [] }
                 },
                 Content = dcContent
